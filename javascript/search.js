33c4eb743e5d32db4c44d6a5bf2d1dea730881a7
CarnageOG <egetashviligiorgi9@gmail.com>
2025-04-29 20:39:12 +0400

    კატეგორიები

--- javascript/search.js
+++ javascript/search.js
@@ -1,7 +1,41 @@
-// ინდუსტრია //
-const dropdown = document.getElementById("industryDropdown");
-  const toggleBtn = dropdown.querySelector(".industry-toggle");
+// Industry Dropdown Toggle
+const industryDropdown = document.getElementById("industryDropdown");
+const industryToggleBtn = industryDropdown.querySelector(".industry-toggle");
 
-  toggleBtn.addEventListener("click", () => {
-    dropdown.classList.toggle("active");
+industryToggleBtn.addEventListener("click", () => {
+  industryDropdown.classList.toggle("active");
+});
+
+// Region Dropdown Toggle
+const regionDropdown = document.getElementById("regionDropdown");
+const regionToggleBtn = regionDropdown.querySelector(".region-toggle");
+
+regionToggleBtn.addEventListener("click", () => {
+  regionDropdown.classList.toggle("active");
+});
+
+// Business model Dropdown Toggle
+const businessDropdown = document.getElementById("businessDropdown");
+const businessToggleBtn = businessDropdown.querySelector(".business-toggle");
+
+businessToggleBtn.addEventListener("click", () => {
+  businessDropdown.classList.toggle("active");
+});
+
+// კატეგორია
+
+// აკავშირებს მთავარ კატეგორიებს ქვეკატეგორიებთან
+document.querySelectorAll('.main-category-checkbox').forEach(category => {
+  category.addEventListener('change', function () {
+    const targetId = this.dataset.target;
+    const group = document.getElementById(targetId);
+    const checkboxes = group.querySelectorAll('input[type="checkbox"]');
+    checkboxes.forEach(cb => cb.checked = this.checked);
   });
+});
+
+// ღილაკის დაკლიკებით ცვლის active კლასს
+function toggleContent() {
+  const container = document.getElementById('filterBox');
+  container.classList.toggle('active');
+}
